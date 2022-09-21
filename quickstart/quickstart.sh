0c914707a7f80446d6e4e3a4607d6e1974fa3172
mrfakename <me@mrfake.name>
2022-09-21 16:28:17 -0700

    quickstart

---
+++ quickstart/quickstart.sh
@@ -0,0 +1,18 @@
+#!/bin.bash
+echo "SMSBot Quickstart by mrfakename"
+echo "Checking for updates..."
+NEWEST_VERSION=$(curl "https://raw.githubusercontent.com/fakerybakery/smsbot/main/src/main.py")
+CURRENT_VERSION=$(cat main.py)
+echo "Checking for updates..."
+if CURRENT_VERSION != NEWEST_VERSION then
+echo "You are on the latest version."
+echo "Starting server..."
+python3 main.py
+else
+echo "An update is available! Now updating files..."
+curl "https://raw.githubusercontent.com/fakerybakery/smsbot/main/src/main.py" >> main.py
+ScriptLoc=$(readlink -f "$0")
+echo "Updated! Restarting script..."
+echo "& * & * & * & * & * & * & *"
+exec "$ScriptLoc"
+fi
